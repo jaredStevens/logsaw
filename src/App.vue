02f697f3685d66560e67df3fdad7b8b00528543c
Jared Stevens <jared.b.stevens@gmail.com>
2019-10-22 11:50:15 -0500

    beginning stages

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
 <template>
   <v-app>
-    <router-view />
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Zoovu Tech Exam</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-avatar tile height="60" width="175">
+        <img :src="image" />
+      </v-avatar>
     </v-app-bar>
-
     <v-content>
       <router-view />
-      <!--<HelloWorld /> -->
     </v-content>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  // components: {
-  //   HelloWorld
-  // },
-  data: () => ({
-    //
-  })
+  data() {
+    return {
+      image: require("@/assets/zoovu-logo.png")
+    };
+  }
 };
 </script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="game">
-    <h1>This is the game page</h1>
-  </div>
+  <v-container>
+    <v-layout text-center row wrap>
+      <v-flex mt-5 mb-4 xs12>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Let's Play!
+        </h1>
+      </v-flex>
+      <v-flex xs12>
+        <v-btn outlined to="/">Home</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
