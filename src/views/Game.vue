748beb22873669e5512a2495f28bac70d28bf4e7
Jared Stevens <jared.b.stevens@gmail.com>
2019-10-23 22:35:44 -0500

    final

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,14 +7,25 @@
         </h1>
       </v-flex>
 
-      <v-flex mt-5 mb-4 xs12 v-model="score">
-        <h1>Score:  {{ score }} %</h1>
+      <v-flex mt-5 mb-4 xs12>
+        <h1>Score:  {{ score || 0 }} seconds</h1>
+      </v-flex>
+
+      <v-flex v-if="findMe" mt-5 mb-4 xs12>
+        <h1>Find Me</h1>
+        <v-avatar
+              tile
+              height="100px"
+              width="100px"
+            >
+              <v-img contain height="75px" :src="findMe.src"></v-img>
+            </v-avatar>
       </v-flex>
 
       <v-flex xs12 mt-5 mb-4>
         <p><strong>Pickup Cards</strong> (Drag Cards below to put them in order.)</p>
-        <v-flex row wrap justify-center>
-          <draggable group="zoovu" @start="drag = true" @end="drag = false">
+        <v-flex class="hidden" row wrap justify-center>
+          <draggable group="zoovu" :list="items" @start="drag = true" @end="drag = false">
             <v-avatar
               tile
               v-for="(item, index) in items"
@@ -22,7 +33,7 @@
               height="100px"
               width="100px"
             >
-              <v-img contain height="75px" :src="item.src"></v-img>
+              <v-img class="mask" contain height="75px" src="@/assets/placeholder.png"></v-img>
             </v-avatar>
           </draggable>
         </v-flex>
@@ -31,7 +42,7 @@
       <v-flex xs12 mt-5 mb-4>
         <p><strong>Zoovu Logo</strong> (Drag cards here to put the logo back together.)</p>
         <v-flex row wrap justify-center>
-          <draggable group="zoovu" @start="drag = true" @end="drag = false">
+          <draggable class="droppable" group="zoovu" :list="logo" @start="drag = true" @end="drag = false" @change="cardRevealed">
             <v-avatar
               tile
               v-for="(item, index) in logo"
@@ -63,7 +74,9 @@ export default {
   data() {
     return {
       drag: false,
-      score: null,
+      score: 0,
+      findMe: null,
+      winner: false,
       items: [
         { name: "o2", src: require("@/assets/3.png"), id: 3 },
         { name: "u", src: require("@/assets/5.png"), id: 5 },
@@ -74,10 +87,48 @@ export default {
       logo: []
     };
   },
+  mounted() {
+    const randoCalrissian = this.getRandomInt(this.items.length)
+    this.findMe = this.items[randoCalrissian]
+  },
   methods: {
     refresh() {
       window.location.reload(true)
     },
+    cardRevealed(card){
+      if(this.score === 0) {
+        setTimeout(this.updateScore, 1000)
+      }
+      if(card.added) {
+        console.log(card.added.element.name)
+        if(card.added.element.name === this.findMe.name) {
+          this.winner = true
+        } else {
+          this.updateScore(10)
+        }
+      }
+    },
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+    updateScore(increment){
+      if(increment) {
+        this.score += increment
+      } else {
+        this.score += 1
+        if(!this.winner) {
+          setTimeout(this.updateScore, 1000)
+        }
+      } if(this.score >= 45) {
+        this.refresh()
+      }
+    }
   }
 };
 </script>
+<style scoped>
+.droppable {
+  height: 75px;
+  width: 100%;
+}
+</style>
